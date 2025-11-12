1305cfe21c8183ce18c0f2053a0e76e0a088ebea
qqacedd <165684496+qqacedd@users.noreply.github.com>
2025-11-12 06:06:39 +0000

    03_07 Exercise

--- explorecali-jpa/src/main/java/com/example/explorecalijpa/repo/TourRepository.java
+++ explorecali-jpa/src/main/java/com/example/explorecalijpa/repo/TourRepository.java
@@ -2,7 +2,15 @@ package com.example.explorecalijpa.repo;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import com.example.explorecalijpa.model.Difficulty;
 import com.example.explorecalijpa.model.Tour;
+import com.example.explorecalijpa.model.TourPackage;
+import java.util.List;
 
 public interface TourRepository extends JpaRepository<Tour, Integer> {
+
+  List<Tour> findByDifficulty(Difficulty difficulty);
+
+  List<Tour> findByTourPackageCode(String code);
+
 }
